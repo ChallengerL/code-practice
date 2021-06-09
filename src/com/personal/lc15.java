8730cbe4a848ca275a0ccada5fc1c1448620f9dd
ChallengerL <lxhwade@163.com>
2021-06-09 17:02:17 +0800

    第二天题目(剩余)

--- src/com/personal/lc15.java
+++ src/com/personal/lc15.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class lc15 {
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSumTarget(int[] nums) {
         Arrays.sort(nums);
         return nSumTarget(nums, 3, 0, 0);
     }
@@ -21,22 +21,26 @@ public class lc15 {
         List<List<Integer>> res = new ArrayList<>();
         if (n < 2 || n > nums.length) return res;
         if (n == 2) {
-            int low = start, high = nums.length - 1;
-            while (low < high) {
-                int sum = nums[low] + nums[high];
-                int left = nums[low];
-                int right = nums[high];
+            int left = start, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
                 if (sum < target) {
-                    while (low < high && nums[low] == left) low++;
+                    left++;
                 } else if (sum > target) {
-                    while (low < high && nums[high] == right) high--;
+                    right--;
                 } else {
                     List<Integer> list = new ArrayList<>();
-                    list.add(left);
-                    list.add(right);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
                     res.add(list);
-                    while (low < high && nums[low] == left) low++;
-                    while (low < high && nums[high] == right) high--;
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
             }
         } else {
@@ -48,6 +52,39 @@ public class lc15 {
                     res.add(list);
                 }
                 while (i < nums.length - 1 && nums[i] == nums[i + 1]) i++;
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return res;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
             }
         }
         return res;
--- src/com/personal/lc300.java
+++ src/com/personal/lc300.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
  **/
 public class lc300 {
 
-    public int lengthOfLIS(int[] nums) {
+    // 动态规划
+    public int lengthOfLIS_(int[] nums) {
         int[] dp = new int[nums.length];
         int res = 0;
         Arrays.fill(dp, 1);
@@ -20,6 +21,26 @@ public class lc300 {
                 }
             }
             res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    // 动态规划 + 二分查找
+    public int lengthOfLIS(int[] nums) {
+        int[] top = new int[nums.length];
+        int res = 0;
+        for (int num : nums) {
+            int left = 0, right = res;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            top[left] = num;
+            if (left == res) res++;
         }
         return res;
     }
